70b46b37797cfa9e9780c4b3eed53fb52b2f7633
NureAlam68 <nurealam151068@gmail.com>
2024-09-22 23:09:10 +0600

    coupon price calculation

--- js/script.js
+++ js/script.js
@@ -4,13 +4,25 @@ const availableSeatEl = document.getElementById('available-seat');
 const totalPriceEl = document.getElementById('total-price');
 const couponInputEl = document.getElementById('coupon-field');
 const couponBtnEl = document.getElementById('coupon-btn');
+const defaultTextEl = document.getElementById('default-text');
+const grandTotalEl = document.getAnimations('grand-total');
 
 
 let totalBooked = [];
 let totalPrice = 0;
 
+
+//seat button function
 function handleSelectSeat(event) {
-    event.classList.add('bg-color-one', 'text-white')
+    const value = event.innerText;
+
+    if(totalBooked.includes(value)) {
+        return alert('Seat already Booked !')
+    }
+    else if(totalBooked.length < 4){
+        event.classList.add('bg-color-one', 'text-white')
+
+        defaultTextEl.classList.add('hidden')
 
         seatSelected.innerHTML += `
         <li class= "text-base flex font-normal justify-between">
@@ -33,4 +45,31 @@ function handleSelectSeat(event) {
             couponInputEl.removeAttribute('disabled')
             couponBtnEl.removeAttribute('disabled')
         }
+    }
+    else {
+        return alert('Maximum seat booked !')
+    }
+    
 }
+
+
+// coupon button function
+document.getElementById('coupon-btn').addEventListener('click', function() {
+    let couponInputValue = couponInputEl.value;
+    let couponSave = 0;
+
+    if(couponInputValue !== "NEW15" && couponInputValue !== "Couple20") {
+        return alert('Your Provided Coupon is not valid !');
+    }
+
+    if(couponInputValue === "NEW15") {
+        couponSave = totalPrice * .15;
+    }
+    else if(couponInputValue === "Couple20") {
+        couponSave = totalPrice * .20;
+    }
+
+    const grandTotalValue = totalPrice - couponSave;
+    const grandTotalEl = document.getElementById('grand-total');
+    grandTotalEl.innerText = grandTotalValue.toFixed(2);
+})
